82aaed7bac6d90d60fabdb9308e42531edd0c5f2
Wil Linssen <wil@linssen.me>
2013-03-16 14:03:20 +0000

    basic static layout for todos

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
+from django.views.generic.simple import direct_to_template
 
 from todo.api import ItemResource
 
@@ -9,6 +10,8 @@ item_resource = ItemResource()
 
 # Root patterns
 urlpatterns = patterns('',
+    (r'^$', direct_to_template, {'template': 'index.html'}),
+
     (r'^api/', include(item_resource.urls)),
 )
 
